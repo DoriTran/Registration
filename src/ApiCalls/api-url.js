ca514b67a34ef1e05104e119803b9767465252f9
DoriTran <tranquocdong308@gmail.com>
2022-10-26 19:23:38 +0700

    netify.toml

--- src/ApiCalls/api-url.js
+++ src/ApiCalls/api-url.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://api-registration-hagaosp5f-doritran.vercel.app"
+// export const API_URL = "https://api-registration-hagaosp5f-doritran.vercel.app"
+export const API_URL = "http://localhost:8000"
