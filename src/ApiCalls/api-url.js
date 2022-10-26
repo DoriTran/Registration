48e17246430da4c3a0c9772b3aba84abf47252fb
DoriTran <tranquocdong308@gmail.com>
2022-10-26 20:43:04 +0700

    update URI

--- src/ApiCalls/api-url.js
+++ src/ApiCalls/api-url.js
@@ -1,2 +1,1 @@
-// export const API_URL = "https://api-registration-hagaosp5f-doritran.vercel.app"
-export const API_URL = "http://localhost:8000"
+export const API_URL = "https://api-registration.vercel.app/"
